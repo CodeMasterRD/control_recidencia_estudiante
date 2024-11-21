55292827d76566cddaea4da0c2943f5049571bcb
Israel vargas <126931996+IsraelDev-var@users.noreply.github.com>
2024-11-21 05:46:21 -0400

    metodo de validar la entrada con la matricula

--- src/Model/RegistroMatricula/HomeDOU.java
+++ src/Model/RegistroMatricula/HomeDOU.java
@@ -1,17 +1,67 @@
 
 package Model.RegistroMatricula;
-
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+import Model.ConexionDB;
+import com.mysql.cj.jdbc.CallableStatement;
+import com.mysql.cj.protocol.Resultset;
+import java.sql.Connection;
+import java.sql.ResultSet;
 
 public class HomeDOU {
     //definimos los atributos
-    public String matricula;
+    public int matricula;
 
-    public String getMatricula() {
+    public int getMatricula() {
         return matricula;
     }
 
-    public void setMatricula(String matricula) {
+    public void setMatricula(int matricula) {
         this.matricula = matricula;
+    }
+    
+    
+    //metodo que validad la entrada
+    // resive un parametro tipo String
+    // retorna un true or false
+    public static boolean validarMatricula(int matricula) throws SQLException, FileNotFoundException{
+        
+        String SQL = "{call validarMatriculaEstudiante(?)}";
+        
+        try { 
+            Connection conexion = ConexionDB.getConexion();
+            CallableStatement callableStatement = (CallableStatement) conexion.prepareCall(SQL);
+            
+            //stablecer el parametro de entrada 
+            
+            callableStatement.setInt(1, matricula);
+            
+            boolean resultada = callableStatement.execute();
+            
+            //procesar resutado
+            if(resultada){
+                try {
+                    ResultSet resultset = callableStatement.getResultSet();
+                    while (resultset.next()) { 
+                        int matriculaObtenida = resultset.getInt("Matricula");
+                        
+                        System.out.println("Matrícula encontrada: " + matriculaObtenida);
+                    }
+                    
+                } catch (SQLException e) {
+                    
+                }
+            }else{
+                
+            
+            }
+            
+            
+            
+        } catch (SQLException e) {
+        }
+        return false;
+        
     }
     
     
