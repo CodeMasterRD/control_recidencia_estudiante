889f008f213c1af2f23881689a1791d9ee5fcbdf
Israel vargas <126931996+IsraelDev-var@users.noreply.github.com>
2024-11-21 04:14:16 -0400

    Inicilizando los eventod de los btns en el contructor HomeCTRL

--- src/Controler/Home/HomeCTRL.java
+++ src/Controler/Home/HomeCTRL.java
@@ -2,6 +2,45 @@
 package Controler.Home;
 
 // Clase controlado del Home
-public class HomeCTRL {
+
+import View.HomeStudentGUI;
+import Model.RegistroMatricula.HomeDOU;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HomeCTRL implements ActionListener{
+    // Instanciar la clase
+    
+    HomeStudentGUI vistaHome = new HomeStudentGUI();
+    HomeDOU homeDOU = new HomeDOU();
+    
+
+    public HomeCTRL( HomeStudentGUI v ) {
+        this.vistaHome = v;
+        //inicilizar los btns
+        this.vistaHome.btn0.addActionListener(this);
+        this.vistaHome.btn1.addActionListener(this);
+        this.vistaHome.btn2.addActionListener(this);
+        this.vistaHome.btn3.addActionListener(this);
+        this.vistaHome.btn4.addActionListener(this);
+        this.vistaHome.btn5.addActionListener(this);
+        this.vistaHome.btn6.addActionListener(this);
+        this.vistaHome.btn7.addActionListener(this);
+        this.vistaHome.btn8.addActionListener(this);
+        this.vistaHome.btn9.addActionListener(this);
+        this.vistaHome.btnEntrar.addActionListener(this);
+        this.vistaHome.btnEliminar.addActionListener(this);
+        
+    }
+    
+    
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
+    
+    
+    
     
 }
--- src/View/HomeStudentGUI.java
+++ src/View/HomeStudentGUI.java
@@ -19,13 +19,13 @@ public class HomeStudentGUI extends javax.swing.JFrame {
         btn1 = new javax.swing.JButton();
         btn0 = new javax.swing.JButton();
         btn9 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
+        btnEntrar = new javax.swing.JButton();
         btn8 = new javax.swing.JButton();
         btn5 = new javax.swing.JButton();
-        jButton8 = new javax.swing.JButton();
+        btnEliminar = new javax.swing.JButton();
         btn7 = new javax.swing.JButton();
         btn4 = new javax.swing.JButton();
-        jButton11 = new javax.swing.JButton();
+        btn6 = new javax.swing.JButton();
         btn3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -66,12 +66,12 @@ public class HomeStudentGUI extends javax.swing.JFrame {
         btn9.setBorder(null);
         jPContenedor.add(btn9, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 420, 100, 60));
 
-        jButton5.setBackground(new java.awt.Color(121, 240, 139));
-        jButton5.setFont(new java.awt.Font("Roboto Black", 0, 18)); // NOI18N
-        jButton5.setForeground(new java.awt.Color(255, 255, 255));
-        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Images/Arrow right.png"))); // NOI18N
-        jButton5.setBorder(null);
-        jPContenedor.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 500, 100, 60));
+        btnEntrar.setBackground(new java.awt.Color(121, 240, 139));
+        btnEntrar.setFont(new java.awt.Font("Roboto Black", 0, 18)); // NOI18N
+        btnEntrar.setForeground(new java.awt.Color(255, 255, 255));
+        btnEntrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Images/Arrow right.png"))); // NOI18N
+        btnEntrar.setBorder(null);
+        jPContenedor.add(btnEntrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 500, 100, 60));
 
         btn8.setBackground(new java.awt.Color(0, 117, 250));
         btn8.setFont(new java.awt.Font("Roboto Black", 0, 18)); // NOI18N
@@ -87,12 +87,12 @@ public class HomeStudentGUI extends javax.swing.JFrame {
         btn5.setBorder(null);
         jPContenedor.add(btn5, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 340, 100, 60));
 
-        jButton8.setBackground(new java.awt.Color(250, 76, 76));
-        jButton8.setFont(new java.awt.Font("Roboto Black", 0, 18)); // NOI18N
-        jButton8.setForeground(new java.awt.Color(255, 255, 255));
-        jButton8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Images/Delete.png"))); // NOI18N
-        jButton8.setBorder(null);
-        jPContenedor.add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 500, 100, 60));
+        btnEliminar.setBackground(new java.awt.Color(250, 76, 76));
+        btnEliminar.setFont(new java.awt.Font("Roboto Black", 0, 18)); // NOI18N
+        btnEliminar.setForeground(new java.awt.Color(255, 255, 255));
+        btnEliminar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/View/Images/Delete.png"))); // NOI18N
+        btnEliminar.setBorder(null);
+        jPContenedor.add(btnEliminar, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 500, 100, 60));
 
         btn7.setBackground(new java.awt.Color(0, 117, 250));
         btn7.setFont(new java.awt.Font("Roboto Black", 0, 18)); // NOI18N
@@ -108,12 +108,12 @@ public class HomeStudentGUI extends javax.swing.JFrame {
         btn4.setBorder(null);
         jPContenedor.add(btn4, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 340, 100, 60));
 
-        jButton11.setBackground(new java.awt.Color(0, 117, 250));
-        jButton11.setFont(new java.awt.Font("Roboto Black", 0, 18)); // NOI18N
-        jButton11.setForeground(new java.awt.Color(255, 255, 255));
-        jButton11.setText("6");
-        jButton11.setBorder(null);
-        jPContenedor.add(jButton11, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 340, 100, 60));
+        btn6.setBackground(new java.awt.Color(0, 117, 250));
+        btn6.setFont(new java.awt.Font("Roboto Black", 0, 18)); // NOI18N
+        btn6.setForeground(new java.awt.Color(255, 255, 255));
+        btn6.setText("6");
+        btn6.setBorder(null);
+        jPContenedor.add(btn6, new org.netbeans.lib.awtextra.AbsoluteConstraints(710, 340, 100, 60));
 
         btn3.setBackground(new java.awt.Color(0, 117, 250));
         btn3.setFont(new java.awt.Font("Roboto Black", 0, 18)); // NOI18N
@@ -130,7 +130,7 @@ public class HomeStudentGUI extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPContenedor, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPContenedor, javax.swing.GroupLayout.DEFAULT_SIZE, 673, Short.MAX_VALUE)
         );
 
         pack();
@@ -140,19 +140,19 @@ public class HomeStudentGUI extends javax.swing.JFrame {
     
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btn0;
-    private javax.swing.JButton btn1;
-    private javax.swing.JButton btn2;
-    private javax.swing.JButton btn3;
-    private javax.swing.JButton btn4;
-    private javax.swing.JButton btn5;
-    private javax.swing.JButton btn7;
-    private javax.swing.JButton btn8;
-    private javax.swing.JButton btn9;
-    private javax.swing.JButton jButton11;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton8;
+    public javax.swing.JButton btn0;
+    public javax.swing.JButton btn1;
+    public javax.swing.JButton btn2;
+    public javax.swing.JButton btn3;
+    public javax.swing.JButton btn4;
+    public javax.swing.JButton btn5;
+    public javax.swing.JButton btn6;
+    public javax.swing.JButton btn7;
+    public javax.swing.JButton btn8;
+    public javax.swing.JButton btn9;
+    public javax.swing.JButton btnEliminar;
+    public javax.swing.JButton btnEntrar;
     private javax.swing.JPanel jPContenedor;
-    private javax.swing.JTextField txtMatricula;
+    public javax.swing.JTextField txtMatricula;
     // End of variables declaration//GEN-END:variables
 }
