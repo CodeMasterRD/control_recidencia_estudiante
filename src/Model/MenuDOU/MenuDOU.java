449cb8eea282fd2f6d615ece9780d8135e04ad6a
Israel vargas <126931996+IsraelDev-var@users.noreply.github.com>
2024-11-22 05:32:19 -0400

    Registra entrada casi listo, cree la clase procedure que contendra alguno procedimiento amacenado

--- src/Model/MenuDOU/MenuDOU.java
+++ src/Model/MenuDOU/MenuDOU.java
@@ -1,16 +1,21 @@
 
 package Model.MenuDOU;
+import Model.ConexionDB;
+import Model.FechaHora.FechaHora;
 import java.io.FileNotFoundException;
 import java.sql.SQLException;
 import Model.RegistroMatricula.HomeDOU;
 import View.HomeStudentGUI;
 import View.Dialogos.DialogoValidado;
+import com.mysql.cj.jdbc.CallableStatement;
+import com.sun.jdi.connect.spi.Connection;
 
 // Clase que menejalos dato del MenuGUI
 
 public class MenuDOU {
     
     private String btnSeleccionado;
+    
     
 
     public MenuDOU() {
@@ -30,20 +35,36 @@ public class MenuDOU {
     
     
     //metodo que registra la entrada del estudiante a la residencia
-    public static boolean registraEntrada() throws SQLException, FileNotFoundException{
-        try {
-            int mat = HomeDOU.getMatricula();
-            System.out.println("Registrando entrada");
-            System.out.println("La matricula a registra la entrada es: ");
-            System.out.println(mat);
+    public static boolean registraEntrada(int matricula, String fechaHora, String estado, String fecha, String hora) throws SQLException, FileNotFoundException{
+        //procedure 
+        String SQL_REGISTRAR_ENTRADA = "{call registraentrada(?,?,?,?,?)}";
+        try(java.sql.Connection conexion = ConexionDB.getConexion();
+                CallableStatement call = (CallableStatement) conexion.prepareCall(SQL_REGISTRAR_ENTRADA)){
             
+            // Convertir LocalDateTime a SQL Date y Time
             
+             // Establecer parámetros del procedimiento almacenado
+            call.setInt(1, matricula); // Matricula
+            call.setTimestamp(2, java.sql.Timestamp.valueOf(fechaHora)); // Fecha y Hora completa
+            call.setString(3, estado); // Estado ('Fuera' o 'Dentro')
+            //call.setDate(4, fecha); // Fecha (DATE)
+            //call.setTime(5, hora); // Hora (TIME)
             
-            
+            // Ejecutar el procedimiento
+            call.execute();
+            System.out.println("Entrada registrada exitosamente.");
             return true;
-        } catch (Exception e) {
+            
+        } catch (SQLException e) {
+            // Manejo de excepciones SQL
+            if ("45000".equals(e.getSQLState())) {
+                System.err.println("Error personalizado: " + e.getMessage());
+            } else {
+                System.err.println("Error SQL: " + e.getMessage());
+            }
             return false;
         }
+       
         
     }
     
@@ -55,8 +76,10 @@ public class MenuDOU {
         
         switch (btnSeleccionado) {
             case "btnEntrada" -> {
-                
-                return registraEntrada();
+                int matricula = HomeDOU.getMatricula();
+//                String fechaHora = FechaHora.getFechaHoraActual();
+//                String estado = 
+//                return registraEntrada( );
             }
             case "btnSalida" -> {
                
---
+++ src/Model/Procedure/ProcedimientoUtils.java
@@ -0,0 +1,8 @@
+
+package Model.Procedure;
+
+public class ProcedimientoUtils {
+    
+    
+    
+}
