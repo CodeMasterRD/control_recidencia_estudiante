a0dc2f97b7de45378e8c89033d4e4025e148de12
Israel vargas <126931996+IsraelDev-var@users.noreply.github.com>
2024-11-23 00:23:40 -0400

    Agregabdo clse DialogoDentroDOU con sus geter y setter

---
+++ src/Model/DialogosDOU/DialogoDentroDOU.java
@@ -0,0 +1,23 @@
+
+package Model.DialogosDOU;
+
+
+
+
+public class DialogoDentroDOU {
+    
+    public static boolean  ok;
+
+    public DialogoDentroDOU() {
+    }
+
+    public static boolean isOk() {
+        return ok;
+    }
+
+    public static void setOk(boolean ok) {
+        DialogoDentroDOU.ok = ok;
+    }
+    
+    
+}
---
+++ src/View/Dialogos/DialogoDentroGUI.java
@@ -0,0 +1,64 @@
+
+package View.Dialogos;
+
+public class DialogoDentroGUI extends javax.swing.JFrame {
+
+    
+    public DialogoDentroGUI() {
+        initComponents();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        btnOk = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
+
+        jPanel1.setBackground(new java.awt.Color(237, 237, 237));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel1.setFont(new java.awt.Font("Roboto Black", 0, 24)); // NOI18N
+        jLabel1.setText("Arvertencia");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, -1, -1));
+
+        jLabel2.setFont(new java.awt.Font("Roboto Light", 0, 14)); // NOI18N
+        jLabel2.setText("No puedes registrar una entrada si ya estas dentro");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 60, -1, -1));
+
+        btnOk.setBackground(new java.awt.Color(0, 117, 250));
+        btnOk.setFont(new java.awt.Font("Roboto Medium", 0, 14)); // NOI18N
+        btnOk.setForeground(new java.awt.Color(255, 255, 255));
+        btnOk.setText("OK");
+        jPanel1.add(btnOk, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 123, -1, 30));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+ 
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    public javax.swing.JButton btnOk;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
